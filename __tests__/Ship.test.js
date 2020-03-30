4054edc4a83f57263f4878dfd2ab383ac0eca1ec
andreihirleata <hirleata.andrei@gmail.com>
2020-03-30 13:41:42 +0100

    created the Ship object and added functionality tests

---
+++ __tests__/Ship.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+const Ship = require("../src/Ship.js");
+
+beforeEach(() => {
+  ship = new Ship("Dover");
+});
+
+describe("Ship", () => {
+  it("can be instantiated", () => {
+    expect(new Ship()).toBeInstanceOf(Object);
+  });
+  it("has a strating port", () => {
+    expect(ship.startingPort).toBe("Dover");
+  });
+});
