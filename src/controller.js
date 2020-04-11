13dc86763d2a4f1df0f6b8e96513bdfeb92357f2
andreihirleata <hirleata.andrei@gmail.com>
2020-04-11 15:26:19 +0100

    added method to render ports and mplementation

--- src/controller.js
+++ src/controller.js
@@ -1,23 +1,39 @@
-(function exportController () {
-class Controller {
-  constructor() {
-    this.initialiseSea();
-  }
-  initialiseSea() {
-    const backgrounds = [`./images/water0.png`, `./images/water1.png`];
-    let backgroundIndex = 0;
-    window.setInterval(() => {
-      document.querySelector(`#viewport`).style.backgroundImage = `url('${
-        backgrounds[backgroundIndex % backgrounds.length]
-      }')`;
-      backgroundIndex += 1;
-    }, 1000);
-  }
+(function exportController() {
+  class Controller {
+    constructor() {
+      this.initialiseSea();
+    }
+    initialiseSea() {
+      const backgrounds = [`./images/water0.png`, `./images/water1.png`];
+      let backgroundIndex = 0;
+      window.setInterval(() => {
+        document.querySelector(`#viewport`).style.backgroundImage = `url('${
+          backgrounds[backgroundIndex % backgrounds.length]
+        }')`;
+        backgroundIndex += 1;
+      }, 1000);
+    }
 
-}
-if (typeof module !== `undefined` && module.exports) {
-  module.exports = Controller;
-} else {
-  window.Controller = Controller;
-}
-}());
+    renderPorts(ports) {
+      const portsElement = document.querySelector('#ports');
+      portsElement.style.width = "0px";
+
+      ports.forEach((port, index) => {
+        const newPortElement = document.createElement('div');
+        newPortElement.dataset.portName = port.name;
+        newPortElement.dataset.portIndex = index;
+        newPortElement.className = 'port';
+
+        portsElement.appendChild(newPortElement);
+
+        const portsElementWidth = parseInt(portsElement.style.width, 10);
+        portsElement.style.width = `${portsElementWidth + 256}px`;
+      });
+    }
+  }
+  if (typeof module !== `undefined` && module.exports) {
+    module.exports = Controller;
+  } else {
+    window.Controller = Controller;
+  }
+})();
